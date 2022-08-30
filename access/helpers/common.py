af8148e9fe88198c90568dffe33dc84670fc2fba
Tomás Santiago <thom.sgonzalez@gmail.com>
2022-08-30 14:06:21 -0500

    Exception handling decorator

---
+++ access/helpers/common.py
@@ -0,0 +1,13 @@
+
+def generic_error(fn):
+    """
+    Decorator for connection and error handling.
+    """
+    def func_wrapper(*args, **kwargs):
+        try:
+            return fn(*args, **kwargs)
+        except Exception as e:
+            error = "Connection error: {}".format_map(e)
+            raise error
+
+    return func_wrapper
