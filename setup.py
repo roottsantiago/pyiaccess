4335777e138afd59abd24e841e314b651cf2e697
Tomás Santiago <thom.sgonzalez@gmail.com>
2022-09-01 22:41:44 -0500

    Library in beta version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyiaccess",
-    version="0.1.3",
+    version="0.1.4b1",
     author="Tomás Gonzalez",
     author_email="thom.sgonzalez@gmail.com",
     description="It is a library that connects to IBM Power Systems (IBM i), with pyodbc and SSH connection.",
