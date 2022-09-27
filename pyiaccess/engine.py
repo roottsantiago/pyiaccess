f9b171dc09f0d2384d88077a847e8a0bf58267de
Tomás Santiago <thom.sgonzalez@gmail.com>
2022-09-27 01:19:35 -0500

    Creation of database engine and sftp

--- pyiaccess/engine.py
+++ pyiaccess/engine.py
@@ -2,6 +2,8 @@
 import os
 from dotenv import load_dotenv
 from pathlib import Path
+from pyiaccess.transfer import SFTPClient
+from pyiaccess.db import IBMClient
 
 
 def set_env(path_env):
@@ -10,3 +12,13 @@ def set_env(path_env):
     """
     dotenv_path = Path(path_env)
     load_dotenv(dotenv_path=dotenv_path)
+
+
+def create_sftp(**kwargs):
+    sftp = SFTPClient(**kwargs)
+    return sftp
+
+
+def create_db(**kwargs):
+    ibm = IBMClient(**kwargs)
+    return ibm
