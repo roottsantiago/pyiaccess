9f32d3b7bcdf335cdba7f0f432964496273240ee
Tomás Santiago <thom.sgonzalez@gmail.com>
2022-09-27 01:18:15 -0500

    String formatting and fix environment variables

--- pyiaccess/transfer.py
+++ pyiaccess/transfer.py
@@ -11,36 +11,37 @@ class SFTPClient(object):
     Class for SSH client
     """
 
-    def __init__(self):
+    def __init__(self, **kwargs):
         self.ssh_client = paramiko.SSHClient()
         self.ssh_client.load_host_keys(
             os.path.expanduser(os.path.join("~", ".ssh", "known_hosts"))
         )
         self.ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        self.ISERIE_HOST = os.getenv("ISERIE_HOST")
-        self.ISERIE_USER = os.getenv("ISERIE_USER")
-        self.ISERIE_PASSWORD = os.getenv("ISERIE_PASSWORD")
-        self.SFTP_PORT = os.getenv("SFTP_PORT")
-        self.SFTP_REMOTE_PATH = os.getenv("SFTP_REMOTE_PATH")
+        self.SFTP_HOST = kwargs.get("hostname", None)
+        print(self.SFTP_HOST)
+        self.SFTP_USER = kwargs.get("username", None)
+        self.SFTP_PASSWORD = kwargs.get("password", None)
+        self.SFTP_PORT = kwargs.get("port", 22)
+        self.SFTP_REMOTE_PATH = kwargs.get("remote_path", None)
 
     def connect(self):
         try:
             self.ssh_client.connect(
-                hostname=self.ISERIE_HOST,
-                username=self.ISERIE_USER,
-                password=self.ISERIE_PASSWORD,
+                hostname=self.SFTP_HOST,
+                username=self.SFTP_USER,
+                password=self.SFTP_PASSWORD,
                 port=self.SFTP_PORT,
             )
-            msg = "{}: {}".format(self.ISERIE_HOST, "CONNECTED")
+            msg = f"{self.SFTP_HOST}: CONNECTED"
             logging.info(msg)
         except paramiko.AuthenticationException as exc:
-            print("Authentication Failed")
+            logging.error("Authentication Failed")
             raise exc
         except paramiko.SSHException as exc:
-            print("SSH Error")
+            logging.error("SSH Error")
             raise exc
         except Exception as exc:
-            print("Unknown Error: %s" % exc)
+            logging.error(f"Unknown Error: {exc}")
             raise str(exc)
 
     def list_files(self):
@@ -48,17 +49,17 @@ class SFTPClient(object):
         sftp.chdir(self.SFTP_REMOTE_PATH)
         filenames = []
         for filename in sftp.listdir_attr():
-            filenames.append("{}".format(filename))
+            filenames.append(f"{filename}")
         sftp.close()
 
         return filenames
 
     def upload(self, path_file):
         local_file_path = path_file
-        remote_path = "{}{}".format(self.SFTP_REMOTE_PATH, ntpath.basename(path_file))
+        remote_path = f"{self.SFTP_REMOTE_PATH}{ntpath.basename(path_file)}"
         ftp_client = self.ssh_client.open_sftp()
         ftp_client.put(local_file_path, remote_path)
         ftp_client.close()
 
-        msg = "TRANSFERRED: {}".format(remote_path)
+        msg = f"TRANSFERRED: {remote_path}"
         logging.info(msg)
